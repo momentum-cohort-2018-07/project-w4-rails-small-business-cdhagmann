3e73017bd89aa8c65246b9b94667f868ea4a046d
Christopher Dean Hagmann <cdhagmann@gmail.com>
2018-08-24 15:59:24 -0400

    Cleaned up code

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,11 +1,5 @@
-require 'csv'
-
 class ItemsController < ApplicationController
   def index
-    # @items = []
-    # CSV.foreach('/Users/cdhagmann/Documents/Momentum/project-w4-rails-small-business-cdhagmann/db/faust_inventory.csv', headers: true) do |row|
-    #   @items << Item.new(row.to_h)
-    # end
     @items = Item.all.where("quantity > 0")
   end
 
